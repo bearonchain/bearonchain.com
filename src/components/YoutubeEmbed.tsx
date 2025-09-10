fa4df186249541bc9b4b29e005d1c163ea368c86
bear on chain <bearonchain@gmail.com>
2025-09-10 22:57:17 +0530

    name change

--- src/components/YoutubeEmbed.tsx
+++ src/components/YoutubeEmbed.tsx
@@ -4,9 +4,9 @@ interface YouTubeEmbedProps {
 
 export default function YouTubeEmbed({ id }: YouTubeEmbedProps) {
   return (
-    <div className="relative aspect-video w-full">
+    <div className="relative w-full overflow-hidden rounded-xl shadow-lg my-8" style={{ paddingTop: "56.25%" }}>
       <iframe
-        className="absolute top-0 left-0 w-full h-full rounded-xl"
+        className="absolute top-0 left-0 w-full h-full"
         src={`https://www.youtube.com/embed/${id}`}
         title="YouTube video"
         frameBorder="0"
